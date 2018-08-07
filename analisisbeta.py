0a362e537401ee12bdb981f2c3851079a95146ae
davidgarcm <david.garcmtz@gmail.com>
2018-08-07 13:01:53 -0500

    Update analisisbeta.py

--- analisisbeta.py
+++ analisisbeta.py
@@ -11,6 +11,6 @@ bd = pd.read_csv("base_prueba.csv", na_values = np.nan)
 
 # Limpiar base y nombrar funciones para seleccionar desercion 
 bd.dropna(how = "all", axis = "columns", inplace =True)
-bd.drop(["edad", "escolaridad", "codigo_postal", "nombre"], axis = 1, inplace =True)
+bd.drop(["nombre"], axis = 1, inplace =True)
 bd.dropna(inplace = True)
 print(bd.dtypes)
