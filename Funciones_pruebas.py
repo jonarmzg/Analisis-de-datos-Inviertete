1681581538ea71b6012d80ffccae8454865a3515
AlmaSahian <40401989+AlmaSahian@users.noreply.github.com>
2018-08-13 17:09:25 -0500

    Funciones para  conversiones de Terman y Raven
    Funciones para convertir los puntajes de pruebas  Terman y Raven

---
+++ Funciones_pruebas.py
@@ -0,0 +1,297 @@
+
+def terman_I(terman_I):
+    if terman_I>=0 and terman_I<8: score_terman_I="DEF"
+    elif  terman_I>=8 and terman_I<10: score_terman_I="INF"
+    elif  terman_I==10 or terman_I==11: score_terman_I="TMB"
+    elif  terman_I==12 or terman_I==13: score_terman_I="TM"
+    elif  terman_I==14: score_terman_I="TMA"
+    elif  terman_I==15: score_terman_I="SUP"
+    elif  terman_I==16: score_terman_I="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_I
+
+def terman_II(terman_II):
+    if terman_II>=0 and terman_II<7: score_terman_II="DEF"
+    elif  terman_II==8: score_terman_II="INF"
+    elif  terman_II==10: score_terman_II="TMB"
+    elif  terman_II>=12 or terman_II<=16: score_terman_II="TM"
+    elif  terman_II==18: score_terman_II="TMA"
+    elif  terman_II==20: score_terman_II="SUP"
+    elif  terman_II==22: score_terman_II="SOB"
+    else : print ("Numero fuera del rango")
+    return score_terman_II
+
+def Terman_III(terman_III):
+    if terman_III>=0 and terman_III<=7: score_terman_III="DEF"
+    elif terman_III>=8 and terman_III<=11: score_terman_III="INF"
+    elif  terman_III==12 or terman_III==13 : score_terman_III="TMB"
+    elif  terman_III>=14 and terman_III<=22 : score_terman_III="TM"
+    elif  terman_III>=23 and terman_III<=26: score_terman_III="TMA"
+    elif  terman_III>=14 and terman_III<=22: score_terman_III="SUP"
+    elif  terman_III>=14 and terman_III<=22: score_terman_III="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_III
+
+def Terman_IV(terman_IV):
+    if terman_IV>=0 and terman_IV<=5: score_terman_IV="DEF"
+    elif terman_IV==6: score_terman_IV="INF"
+    elif  terman_IV>=7 and terman_IV<=9 : score_terman_IV="TMB"
+    elif  terman_IV>=10 and terman_IV<=13 : score_terman_IV="TM"
+    elif  terman_IV==14 or terman_IV==15: score_terman_IV="TMA"
+    elif  terman_IV==16 or terman_IV==17: score_terman_IV="SUP"
+    elif  terman_IV==18: score_terman_IV="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_IV
+
+def Terman_V(terman_V):
+    if terman_V>=0 and terman_V<=5: score_terman_V="DEF"
+    elif terman_V==6 : score_terman_V="INF"
+    elif  terman_>=8 and terman_V<=10 : score_terman_V="TMB"
+    elif  terman_V>=12 and terman_V<=14 : score_terman_V="TM"
+    elif  terman_V>=16 and terman_V<=18: score_terman_V="TMA"
+    elif  terman_V==22: score_terman_V="SUP"
+    elif  terman_V==24: score_terman_V="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_V
+
+def Terman_VI(terman_VI):
+    if terman_VI>=0 and terman_VI<=4: score_terman_VI="DEF"
+    elif terman_VI==5 or terman_VI==6: score_terman_VI="INF"
+    elif  terman_VI==7 or terman_VI==8: score_terman_VI="TMB"
+    elif  terman_VI>=9 and terman_VI<=14 : score_terman_VI="TM"
+    elif  terman_VI>=15 and terman_VI<=17: score_terman_VI="TMA"
+    elif  terman_VI>=18 and terman_VI<=19: score_terman_VI="SUP"
+    elif  terman_VI==20: score_terman_VI="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_VI
+
+def Terman_VII(terman_VII):
+    if terman_VII>=0 and terman_VII<=4: score_terman_VII="DEF"
+    elif terman_VII==5 : score_terman_VII="INF"
+    elif  terman_VII>=6 and terman_VII<=8: score_terman_VII="TMB"
+    elif  terman_VII>=9 and terman_VII<=15 : score_terman_VII="TM"
+    elif  terman_VII>=16 and terman_VII<=17: score_terman_VII="TMA"
+    elif  terman_VII==18: score_terman_VII="SUP"
+    elif  terman_VII>=19 and terman_VII<=20: score_terman_VII="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_VII
+
+def Terman_VIII(terman_VIII):
+    if terman_VIII>=0 and terman_VIII<=5: score_terman_VIII="DEF"
+    elif  terman_VIII==6: score_terman_VIII="INF"
+    elif  terman_VIII==7 : score_terman_VIII="TMB"
+    elif  terman_VIII>=8 and terman_VIII<=12 : score_terman_VIII="TM"
+    elif  terman_VIII>=13 and terman_VIII<=14: score_terman_VIII="TMA"
+    elif  terman_VIII>=15 and terman_VIII<=16: score_terman_VIII="SUP"
+    elif  terman_VIII==17: score_terman_VIII="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_VIII
+
+
+def Terman_IX(terman_IX):
+    if terman_IX>=0 and terman_IX<=6: score_terman_IX="DEF"
+    elif terman_IX==7 or terman_IX==8: score_terman_IX="INF"
+    elif  terman_IX==9: score_terman_IX="TMB"
+    elif  terman_IX>=10 and terman_IX<=15 : score_treman_IX="TM"
+    elif  terman_IX==16: score_terman_IX="TMA"
+    elif  terman_IX==17: score_terman_IX="SUP"
+    elif  terman_IX==18: score_terman_IX="SOB"
+    else: print ("Numero fuera del rango")
+    return score_treman_IX
+
+def Terman_X(terman_X):
+    if terman_X>=0 and terman_X<=4: score_terman_X="DEF"
+    elif  terman_X==6: score_terman_X="INF"
+    elif  terman_X==8: score_terman_X="TMB"
+    elif  terman_X>=10 and terman_X<=14 : score_terman_X="TM"
+    elif  terman_X==16: score_terman_X="TMA"
+    elif  terman_X==18: score_terman_X="SUP"
+    elif  terman_X>=20 and terman_X<=22 : score_terman_X="SOB"
+    else: print ("Numero fuera del rango")
+    return score_terman_X
+
+def raven_score(edad, score):
+    if edad==12:
+        if score>=0 and score<=14:
+            percentil=5
+        elif score <=24:
+            percentil=10
+        elif score <=33:
+            percentil=25
+
+        elif score <39:
+            percentil=51
+        elif score==39:
+            percentil=50
+        elif score <43:
+            percentil=52
+
+        elif score >=53:
+            percentil=99
+        elif score >=47:
+            percentil=90
+        elif score >=43:
+            percentil=75
+
+
+        else :print("Score fuera del rango")
+    elif edad==13 or edad==14:
+        if score>=0 and score<=17:
+            percentil=5
+        elif score <=27:
+            percentil=10
+        elif score <=34:
+            percentil=25
+
+        elif score <40:
+            percentil=51
+        elif score ==40:
+            percentil=50
+        elif score <45:
+            percentil=52
+
+        elif score >=54:
+            percentil=99
+        elif score >=49:
+            percentil=90
+        elif score >=45:
+            percentil=75
+
+
+
+        else: print("Score fuera del rango")
+    elif edad==15 or edad==16:
+        if score>=0 and score<=19:
+            percentil=5
+        elif score <=29:
+            percentil=10
+        elif score <=35:
+            percentil=25
+
+        elif score <41:
+            percentil=51
+        elif score ==41:
+            percentil=50
+        elif score <46:
+            percentil=52
+
+        elif score >=55:
+            percentil=99
+        elif score >=50:
+            percentil=90
+        elif score >=46:
+            percentil=75
+
+
+        else: print("Score fuera del rango")
+    elif edad==17:
+        if score>=0 and score<=28: percentil=5
+        elif score <=35: percentil=10
+        elif score <=39: percentil=25
+
+        elif score <45: percentil=51
+        elif score ==45: percentil=50
+        elif score <49: percentil=52
+
+        elif score >=56: percentil=99
+        elif score >=52: percentil=90
+        elif score >=49: percentil=75
+
+
+        else :print("Score fuera del rango")
+    elif edad==18:
+        if score>=0 and score<=29: percentil=5
+        elif score <=36: percentil=10
+        elif score <=42: percentil=25
+
+        elif score <46: percentil=51
+        elif score ==46: percentil=50
+        elif score <50: percentil=52
+
+        elif score >=57: percentil=99
+        elif score >=53: percentil=90
+        elif score >=50: percentil=75
+
+        else: print("Score fuera del rango")
+    elif edad==19:
+        if score>=0 and score<=30: percentil=5
+        elif score <=37: percentil=10
+        elif score <=42: percentil=25
+
+        elif score <47: percentil=51
+        elif score ==47: percentil=50
+        elif score <51: percentil=52
+
+        elif score >=57: percentil=99
+        elif score >=54: percentil=90
+        elif score >=51: percentil=75
+
+
+        else :print("Score fuera del rango")
+    elif edad==20 or edad==21:
+        if score>=0 and score<=30: percentil=5
+        elif score <=37: percentil=10
+        elif score <=43: percentil=25
+
+        elif score <47: percentil=51
+        elif score ==47: percentil=50
+        elif score <51: percentil=52
+
+        elif score >=58: percentil=99
+        elif score >=54: percentil=90
+        elif score >=51: percentil=75
+
+
+        else: print("Score fuera del rango")
+    elif edad==22 or edad==65:
+        if score>=0 and score<=31: percentil=5
+        elif score <=38: percentil=10
+        elif score <=44: percentil=25
+
+        elif score <48: percentil=51
+        elif score ==48: percentil=50
+        elif score <52: percentil=52
+
+        elif score >=59: percentil=99
+        elif score==58:  percentil=95
+        elif score >=55: percentil=90
+
+
+
+        elif score >=52: percentil=75
+
+
+
+        else: print ("Edad fuera del rango")
+
+    claves={95:['Superior', 'I'],
+    90:['Superior al término medio', 'II+'],
+    75:['Superior al término medio', 'II'],
+    52:['Término medio', 'III+'],
+    50:['Término medio', 'III'],
+    51:['Término medio', 'III-'],
+    25:['Inferior al término medio', 'IV+'],
+    10:['Inferior al término medio', 'IV'],
+    5:['Deficiente', 'V']}
+
+    return clave[percentil]
+
+
+
+def Terman_puntaje_CI(puntaje_total):
+    #lista de patrones de cambio del CI en prueba Terman
+    lista_pat=[3,2,3,2,4,2,3,1,4,3,3,3,3,2,2,4,3,3,3,4,2,4,4,4,4,4,4,4,4,2,3,4,1,3,3,2,2,3,3,2,1]
+    #sum=0
+    #Generando nuevo lista que se utilizará para conversiones de puntaje a CI
+    # for x in range(0,len(lista_pat)-1):
+    #     sum+=lista_pat[x]
+    #     lista_sumas.append(sum)
+    lista_conv=[0, 3, 5, 8, 10, 14, 16, 19, 20, 24, 27, 30, 33, 36, 38, 40, 44, 47, 50, 53, 57, 59, 63, 67, 71, 75, 79, 83, 87, 91, 93, 96, 100, 101, 104, 107, 109, 111, 114, 117, 119,120]
+    #Conversion del punyaje total al CI
+    dif= puntaje_total-66
+    for x in range(0,len(lista_conv)):
+        if(lista_conv[x]>=dif):
+            CI=79+x
+            print(CI)
+            break
+    return CI
